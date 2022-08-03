8604564fa25ade7295d3e729099e051257f2e1e7
reezayn <rijanshrestha98450@gmail.com>
2022-08-03 23:39:04 +0545

    removed error page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Blogs from "./components/Blogs";
 import About from "./components/About";
 import Home from "./components/Home";
 import SharedLayout from "./components/SharedLayout";
-import ErrorPage from "./components/ErrorPage";
 
 
 function App() {
@@ -21,7 +20,6 @@ function App() {
         <Route path="about" element={<About />}/>
         <Route path="blogs" element={<Blogs />} />
         <Route path="exploreTab" element={<ExploreTab />} />
-        <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
     </BrowserRouter>
