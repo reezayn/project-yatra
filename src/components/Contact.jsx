81407cfbd727f66113a2e55f2cac768d3d74b56a
reezayn <rijanshrestha98450@gmail.com>
2022-08-04 00:39:56 +0545

    another commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,7 +45,7 @@ const Contact = () => {
                   <textarea placeholder='Your Message' rows='3' className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-[#5651e5]'/>
                 </div>
                 
-                <button className='inline-block self-end'>Submit</button>
+                <button className='inline-block self-end'><a href="/">Submit</a></button>
 
               </form>
             </div>
